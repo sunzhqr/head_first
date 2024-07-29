4fd6ded4ed90cde678a0ec303b930632acf92ff9
sunzhqr <sanzhar_myrzash@mail.ru>
2024-07-29 14:21:41 +0500

    using defined type of struct with functions

--- part8/structs.go
+++ part8/structs.go
@@ -13,16 +13,17 @@ type car struct {
 }
 
 func main() {
-	var porsche car
-	porsche.name = "Porsche 911 R"
-	porsche.topSpeed = 323
-	fmt.Println("Name: ", porsche.name)
-	fmt.Println("Top speed: ", porsche.topSpeed)
-	bolts := part{
-		description: "Bolt 1",
-		count:       1,
-	}
-	fmt.Println("Description: ", bolts.description)
-	fmt.Println("Count: ", bolts.count)
+	showInfo(car{"Aventador", 455})
+	fmt.Printf("% 	v\n", minimiumOrder("Hex bolts"))
+}
 
+func showInfo(c car) {
+	fmt.Println("This is ", c.name, "with top speed of ", c.topSpeed)
+}
+
+func minimiumOrder(descripiton string) part {
+	var p part
+	p.description = descripiton
+	p.count = 100
+	return p
 }
